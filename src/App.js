4cd454146ba2fc2688a65efc5484eb758288e273
NurmuhammadovHojiakbar <pythoncoderuz@gmail.com>
2022-01-11 17:31:29 +0500

    Routes fixed

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import './App.css';
 import { RouteComponents } from './Data/RouteComponents';
 import Layout from './Layout/Layout';
 import NotFound from './Pages/404/NotFound';
+import SignIn from './Pages/SignIn/SignIn';
 
 function App() {
+
+  const token = true
+
   return (
     <div className="App">
       <Routes>
         {
+          token ? <Route path="/" element={<SignIn />} /> :
           RouteComponents.map(c=>(
-            <Route path={c.path} element={c.path === "/" ? c.element : <Layout>{c.element}</Layout>} />
+            <Route path={c.path} element={<Layout>{c.element}</Layout>} />
           ))
+          
         }
         <Route path="*" element={<NotFound />} />
       </Routes>
