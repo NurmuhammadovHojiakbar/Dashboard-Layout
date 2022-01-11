0dd13607a45546a045b4ea1ad9c923a3a9ad9e0e
NurmuhammadovHojiakbar <pythoncoderuz@gmail.com>
2022-01-12 03:57:20 +0500

    login wipt

--- src/Pages/SignIn/SignIn.js
+++ src/Pages/SignIn/SignIn.js
@@ -1,6 +1,17 @@
+import { Box } from "@mui/material"
+
 const SignIn = () => {
+
+    const loginStyles = {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+    }
+
     return (
-        <></>
+        <Box component="div" sx={loginStyles}>
+        </Box>
     );
 }
  
